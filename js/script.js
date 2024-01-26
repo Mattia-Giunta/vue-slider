dbffe2aa6ef486970e8c364172f0feecac1781bc
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-01-26 18:40:29 +0100

    inserito i bonus e terminato esercizio

--- js/script.js
+++ js/script.js
@@ -15,71 +15,59 @@
 
 
 
-// const slides = [
-// {
-//     image: 'img/01.webp',
-//         title: 'Marvel\'s Spiderman Miles Morale',
-//         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-//     }, {
-//         image: 'img/02.webp',
-//         title: 'Ratchet & Clank: Rift Apart',
-//         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-//     }, {
-//         image: 'img/03.webp',
-//         title: 'Fortnite',
-//         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//     }, {
-//         image: 'img/04.webp',
-//         title: 'Stray',
-//         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-//     }, {
-//         image: 'img/05.webp',
-//         title: "Marvel's Avengers",
-//         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-//     }
-// ];
-
-
 
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
+
+        // variabile per dare un valore da inserire
         currentImage: 0,
+        // variabile per dare un valore al setInterval e al clearInterval
+        autoScroll: null,
+
         slides: [
             {
             image: 'img/01.webp',
             title: 'Marvel\'s Spiderman Miles Morale',
             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-            }, 
+            },
             {
             image: 'img/02.webp',
             title: 'Ratchet & Clank: Rift Apart',
             text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-            }, 
+            },
             {
             image: 'img/03.webp',
             title: 'Fortnite',
             text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            }, 
+            },
             {
             image: 'img/04.webp',
             title: 'Stray',
             text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            }, 
+            },
             {
             image: 'img/05.webp',
             title: "Marvel's Avengers",
             text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
             },
         ],
-        
+
       }
     },
+    // permette di far partire il setInterval() appena carichi la pagina
+    created(){
+
+        // finzione custom
+        this.activeInterval()
+
+    },
     methods:{
+        // funzione custom per passare all'immagine precedente e ripartire 
         prevIMG(){
-            
+
             this.currentImage = this.currentImage - 1;
 
             if( this.currentImage < 0 ){
@@ -89,6 +77,7 @@ const { createApp } = Vue
             }
 
         },
+        // funzione custom per passare all'immagine dopo e ripartire da zero
         nextIMG(){
 
             this.currentImage = this.currentImage + 1;
@@ -96,9 +85,32 @@ const { createApp } = Vue
             if( this.currentImage > this.slides.length - 1 ){
 
                 this.currentImage = 0;
-                
+
             }
+        },
+        // funzione custom per temporizzare il cambio dell'immagine
+        activeInterval(){
+
+            this.autoScroll = setInterval( () => {
+
+                this.nextIMG()
+
+            }, 3000)
+        },
+        // funzione custom per attivare il clearInterval
+        stopInterval(){
+
+            clearInterval(this.autoScroll)
+
+        },
+        // funzione custom per legare la currentImage all'immagine selezzionata nel thum
+        changeImage(index){
+
+           this.currentImage = index;
+
         }
 
     }
   }).mount('#app')
+
+//   Terminato esercizio
